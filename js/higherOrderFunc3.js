ffbe7e048648030dd18a3b1d1acc7d6b26845c66
Deni Supriyatna <denisupriyatna01@gmail.com>
2021-03-28 21:00:35 +0700

    lanjutan

--- js/higherOrderFunc3.js
+++ js/higherOrderFunc3.js
@@ -1,16 +1,67 @@
 // ambil semua element video
 const liJudul = document.getElementsByTagName('li');
 // console.log(liJudul.length);
-
+const videos = Array.from( document.querySelectorAll('[data-duration]'));
 // pilih yang hanya javascript lanjutan
-for (let i = 0; i < liJudul.length; i++) {
-    if (liJudul[i].value ) {
-        console.log("A");
-    }    
-}
+const jsLanjut = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
 
 // ambil masing masing durasi video
-console.log(liJudul[1].getAttributeNames("data-duration").innerText); 
+.map(item => item.dataset.duration)
+
+// ambil durasi, ubah jadi int / float , menit ubah jadi detik
+.map(waktu => {
+    // 10:30 => [10, 30]
+    const parts = waktu.split(':').map(part => parseFloat(part));
+    return (parts[0] * 60) + parts[1];
+})
+
+// jumlahkan semua detik
+.reduce((total, detik) => total + detik);
+
+// ubah format nya jadi jam, menit, detik 
+const jam = Math.floor(jsLanjut / 3600);
+let sisaDetik = jsLanjut - (jam * 3600);
+
+const menit = Math.floor(sisaDetik/60);
+const detik = sisaDetik - (menit * 60);
+
+const jumlahVideio = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN')).length;
+
+// simpan kedalam DOM
+const spanJumlahVideo = document.querySelector('.jumlah-video');
+const spanTotalDurasi = document.querySelector('.total-durasi');
+
+spanJumlahVideo.textContent = `${jumlahVideio} video`;
+// spanJumlahVideo.innerText = 2;
+spanTotalDurasi.textContent = `${jam} jam, ${menit} menit, ${detik} detik`;;
+// spanTotalDurasi.innerText = "20:30";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < liJudul.length; i++) {
+//     if (liJudul[i].value ) {
+//         console.log("A");
+//     }    
+// }
+
+// ambil masing masing durasi video
+// console.log(liJudul[1].getAttributeNames("data-duration").innerText); 
 
 // console.log(liJudul);
 
@@ -31,10 +82,3 @@ console.log(liJudul[1].getAttributeNames("data-duration").innerText);
 // jumlahkan semua detik
 
 // ubah formatnya menjadi jam, menit, detik
-
-// simpan kedalam DOM
-const spanJumlahVideo = document.querySelector('.jumlah-video');
-const spanTotalDurasi = document.querySelector('.total-durasi');
-
-spanJumlahVideo.innerText = 2;
-spanTotalDurasi.innerText = "20:30";
